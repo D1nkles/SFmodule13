7ddefac4deb42004a247e395ed6eb5d50974a743
Ilia <isinetov@mail.ru>
2024-05-12 22:01:17 +0400

    Добавьте файлы проекта.

---
+++ Task13.1.4/Task13.1.4.cs
@@ -0,0 +1,23 @@
+﻿class Program 
+{
+    static bool IsSorted(int[] arr) 
+    {
+        bool isSorted = true;
+
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            if (!(arr[i] < arr[i + 1])) 
+            {
+                isSorted = false;
+                return isSorted;
+            }
+        }
+        return isSorted;
+    }
+    static void Main(string[] args) 
+    {
+        int[] arr = { 1 , 2, 3, 99, 5, 6, 7, 8, 9 };
+        bool isSorted = IsSorted(arr);
+
+    }
+}
