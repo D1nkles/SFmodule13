d7de8a4026da132c7bf6ad721f264da4a02c2e72
Ilia <isinetov@mail.ru>
2024-05-12 22:47:52 +0400

    Задание 13.2.5. Дано два массива:
    var months = new []
    {
       "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
    };
    
    var numbers = new []
    {
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
    };
    Сохраните их значения в ArrayList так, чтобы после строкового названия месяца шёл его числовой номер.

---
+++ Task13.2.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        var months = new[]
+        {
+            "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        };
+
+        var numbers = new[]
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+        };
+
+        ArrayList arrList = new ArrayList();
+        for (int i = 0; i < months.Length && i < numbers.Length ; i++) 
+        {
+            arrList.Add(months[i]);
+            arrList.Add(numbers[i]);
+        }
+
+        foreach (object element in arrList) 
+        {
+            Console.Write($"{element} ");
+        }
+        
+    }
+}
