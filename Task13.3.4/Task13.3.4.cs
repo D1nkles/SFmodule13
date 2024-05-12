b435588b0f4127eecc4f23a044d08359fa308956
Ilia <isinetov@mail.ru>
2024-05-13 00:06:30 +0400

    Задание 13.3.4. Напишите метод для нашей телефонной книги, который получает на вход список и новый контакт, и добавляет его, только если он уникален.

---
+++ Task13.3.4/Task13.3.4.cs
@@ -0,0 +1,36 @@
+﻿class Program 
+{
+    static void AddUnique(Contact newContact, List<Contact> phoneBook) 
+    {
+        bool isUnique = true;
+        foreach(Contact contact in phoneBook) 
+        {
+            if(contact.Name == newContact.Name) 
+                isUnique = false;
+        }
+
+        if (isUnique) 
+            phoneBook.Add(newContact);
+
+        phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+        foreach(Contact contact in phoneBook) 
+        {
+            Console.WriteLine($"{contact.Name}  {contact.PhoneNumber}  {contact.Email}");
+        }
+    }
+    static void Main(string[] args) 
+    {
+        List<Contact> phoneBook = new List<Contact>() 
+        {
+            new Contact("Сергей", 098582916, "gagwe@mail.com"),
+            new Contact("Андрей", 62384786, "kuyk@mail.com"),
+            new Contact("Олег", 71631612916, "vcmdg@mail.com"),
+            new Contact("Роман", 9753531916, "jtyukty@mail.com")
+        };
+
+        Contact newContact = new Contact("Григорий", 1416367, "kiuylk@mail.com");
+
+        AddUnique(newContact, phoneBook);
+    }
+}
