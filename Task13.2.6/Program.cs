08673f9638d7fec781e2c59ddc9089a76da3780b
Ilia <isinetov@mail.ru>
2024-05-12 23:04:59 +0400

    Задание 13.2.6. Напишите метод, который на вход принимает любой Arraylist input, а на выходе выдает другой Arraylist с двумя элементами, где первый — число (сумма целочисленных элементов input), а второй — строка (текст, составленный из строковых элементов input).

---
+++ Task13.2.6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+class Program 
+{
+    static ArrayList GroupIntString(ArrayList input) 
+    {
+        ArrayList output = new ArrayList();
+        int element1 = 0;
+        string element2 = "";
+        foreach (var inputElement in input)
+        {
+            if (inputElement is int) 
+            {
+                element1 += (int)inputElement;
+            }
+
+            if (inputElement is string)
+            {
+                element2 += inputElement;
+            }
+        }
+        output.Add(element1);
+        output.Add(element2);
+        return output;
+    }
+    static void Main(string[] args) 
+    {
+        ArrayList arrayList = new ArrayList() 
+        {
+            22, "я абоб", 52, 682, "ус, х", new Exception(), "ехе", 4 
+        };
+
+        ArrayList arrayList2 = GroupIntString(arrayList);
+
+        foreach (object element in arrayList2) 
+        {
+            Console.Write(element + "    ");
+        }
+    }
+}
