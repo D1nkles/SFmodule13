5fd4a4d87a36e37494e444a519a710958276882b
Ilia <isinetov@mail.ru>
2024-05-13 20:46:30 +0400

    Задания 13.4.4.

---
+++ Task13.4.4/Contact.cs
@@ -0,0 +1,11 @@
+﻿public class Contact 
+{
+    public Contact(long phoneNumber, String email)
+    {
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public long PhoneNumber { get; set; }
+    public String Email { get; set; }
+}
---
+++ Task13.4.4/Task13.4.4.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    static void WriteDictionary(Dictionary<string, Contact> phoneBook) 
+    {
+        foreach (var contact in phoneBook)
+        {
+            Console.WriteLine(contact.Key + " " + contact.Value.PhoneNumber + " " + contact.Value.Email);
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] args) 
+    {
+        Dictionary<string, Contact> phoneBook = new Dictionary<string, Contact>()
+        {
+            ["Сергей"] = new Contact(6346263142, "gagwe@mail.com"),
+            ["Андрей"] = new Contact(9768457634, "kuyk@mail.com")
+        };
+
+        WriteDictionary(phoneBook);
+
+        phoneBook.TryAdd("Никита", new Contact(761421253, "myghopewjp@mail.com"));
+
+        WriteDictionary(phoneBook);
+
+        if (phoneBook.TryGetValue("Никита", out Contact contactToChange))
+            contactToChange.PhoneNumber = 511266462;
+       
+        WriteDictionary(phoneBook);
+    }
+}
