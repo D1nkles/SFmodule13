87295acdecae07a244c0ee796e1aafc261733203
Ilia <isinetov@mail.ru>
2024-05-13 19:35:37 +0400

    Задание 13.3.11. Вспомните, какие полезные методы предоставляет HashSet<T>, и доработайте программу из предыдущего задания, чтобы она:
    сообщала, есть ли в предложении цифры;
    подсчитывала количество уникальных символов без знаков препинания (пробел считать знаком препинания).

--- Task13.3.10/Program.cs
+++ Task13.3.10/Program.cs
@@ -2,10 +2,25 @@
 {
     static void Main(string[] args) 
     {
+        bool hasNum;
         string sentence = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
-        char[] arr = sentence.ToCharArray();
+        char[] arrSentence = sentence.ToCharArray();
 
-        HashSet<char> set = new HashSet<char>(arr);
-        Console.WriteLine(set.Count);
+        char[] nums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        char[] marks = { ' ', ',', '.', '!', '?', ';', ':', '-' };
+
+        HashSet<char> set = new HashSet<char>(arrSentence);
+        
+        hasNum = set.Overlaps(nums);
+        if (hasNum)
+            Console.WriteLine("В колллекции есть цифры.");
+        else
+            Console.WriteLine("В коллекции нет цифр.");
+
+        Console.WriteLine($"Всего уникальных символов в предложении {set.Count}");
+
+        set.ExceptWith(marks);
+        Console.WriteLine($"Уникальных символов без знаков препинания: {set.Count}");
     }
 }
