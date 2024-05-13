ac21db0954103f76693546a21dda96bfc9ff2e13
Ilia <isinetov@mail.ru>
2024-05-13 19:00:31 +0400

    Задание 13.3.10. Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.

---
+++ Task13.3.10/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        string sentence = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+        char[] arr = sentence.ToCharArray();
+
+        HashSet<char> set = new HashSet<char>(arr);
+        Console.WriteLine(set.Count);
+    }
+}
